e78194e53ad14ef0e8161cde2053da614555f001
brachsmithi <bsmithi.impaired@gmail.com>
2022-06-22 21:39:02 -0400

    change RestaurantList to use redux

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,6 +1,7 @@
 import {useEffect} from 'react';
+import {connect} from 'react-redux';
 
-export default function RestaurantList({loadRestaurants, restaurants}) {
+export function RestaurantList({loadRestaurants, restaurants}) {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
@@ -12,3 +13,9 @@ export default function RestaurantList({loadRestaurants, restaurants}) {
     </ul>
   );
 }
+
+const mapStateToProps = state => ({
+  restaurants: state.restaurants.records,
+});
+
+export default connect(mapStateToProps)(RestaurantList);
