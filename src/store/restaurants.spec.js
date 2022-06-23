894e61dd7dc3e3494fdf55be09487527e4e27bf1
brachsmithi <bsmithi.impaired@gmail.com>
2022-06-23 12:37:05 -0400

    default to no loading error in state

--- src/store/restaurants.spec.js
+++ src/store/restaurants.spec.js
@@ -5,14 +5,20 @@ import {loadRestaurants} from './restaurants/actions';
 
 describe('restaurants', () => {
   describe('initially', () => {
-    it('does not have the loading flag set', () => {
+    let store;
+    beforeEach(() => {
       const initialState = {};
-      const store = createStore(
+      store = createStore(
         restaurantsReducer,
         initialState,
         applyMiddleware(thunk),
       );
+    });
+    it('does not have the loading flag set', () => {
       expect(store.getState().loading).toEqual(false);
+    });
+    it('does not have the error flag set', () => {
+      expect(store.getState().loadError).toEqual(false);
     });
   });
   describe('loadRestaurants action', () => {
