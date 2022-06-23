3b7148a351912c0b135d222bbc9bd27c5189628a
brachsmithi <bsmithi.impaired@gmail.com>
2022-06-22 21:53:12 -0400

    Create redux tree

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import RestaurantScreen from './components/RestaurantScreen'
+import {Provider} from 'react-redux';
+import store from './store';
+import RestaurantScreen from './components/RestaurantScreen';
 
 export default function App() {
   return (
-      <div>
-        <RestaurantScreen />
-      </div>
+    <Provider store={store}>
+      <RestaurantScreen />
+    </Provider>
   );
 }
